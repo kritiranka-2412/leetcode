5115e13f728ba8bb61ee385df080f3194ab80188
kritiranka-2412 <kritiranka24@gmail.com>
2026-09-12 23:59:05 +0530

    Time: 2 ms (100%), Space: 115.8 MB (29.64%) - LeetHub

---
+++ 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.java
@@ -0,0 +1,15 @@
+class Solution {
+    public long zeroFilledSubarray(int[] nums) {
+        long ans =0;
+        long count =0 ;
+        for(int i:nums){
+            if(i==0){
+                count++;
+                ans+=count;
+            }
+            else
+            count=0;
+        }
+        return ans;
+    }
+}
